d6dffa2d8ecdd6db61e213935d42aace24851521
AlexandruAcea <alex.acea98@gmail.com>
2019-04-18 01:44:28 +0300

    yolo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,70 @@
-import React from "react"
+import React, { Component } from "react"
 import { Link } from "gatsby"
+import "../styles/index.css"
+import logo from "../images/logo.svg"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+class IndexPage extends React.Component {
+  state = {
+    titles: [
+      "ace",
+      "alexandru",
+      "alexacea",
+      "the ace",
+      "ya boi",
+      "thelegend27",
+    ],
+    title: "ace",
+    count: 0,
+  }
+
+  tick() {
+    if (this.state.count !== this.state.titles.length - 1)
+      this.setState(prevState => ({
+        title: this.state.titles[prevState.count],
+        count: prevState.count + 1,
+      }))
+    else
+      this.setState(prevState => ({
+        title: this.state.titles[prevState.count],
+        count: 0,
+      }))
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => this.tick(), 1000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval)
+  }
+
+  render() {
+    return (
+      <div className="background">
+        <SEO title="Home" />
+        <div className="inner">
+          <img id="aceLogo" src={logo} />
+
+          <div className="text">
+            <div id="title">
+              <h1>Hello,</h1>
+              <div id="smartTitle">
+                <h1>I'm</h1>
+                <div id="smart">
+                  <h1>{this.state.title}</h1>
+                </div>
+                <h1>.</h1>
+              </div>
+
+              <p>I BUILD ALL KINDS OF COOL STUFF</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
 
 export default IndexPage
